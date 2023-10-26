04a374f0d1841aa3b6e427291d928147992ae55b
Natália Santos <tanatelha.dados@gmail.com>
2023-10-26 16:16:50 -0300

    Atualização JS

--- script-experimentos.js
+++ script-experimentos.js
@@ -1,33 +1,16 @@
-const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
-
 /*Responsividade usando JS*/
-function adjustSlidesPerView() {
-        if (window.innerWidth <= 400) {
-            document.querySelector('.swiper-container').setAttribute('slides-per-view', '1.5');
-        } else {
-            document.querySelector('.swiper-container').setAttribute('slides-per-view', '2.5');
-        }
-    }
+// Responsividade usando JS
+const swiperContainers = document.querySelectorAll('.swiper-container');
+const header = document.querySelector('.header')
 
-    // Chame a função quando a página carregar e quando a janela for redimensionada
-    window.addEventListener('load', adjustSlidesPerView);
-    window.addEventListener('resize', adjustSlidesPerView); 
+swiperContainers.forEach(function(swiper) {
+  // Execute a lógica de responsividade quando a página é carregada
+  const larguraDaJanela = window.innerWidth;
+
+  if (larguraDaJanela <= 750) {
+    header.classList.add("selecionado")
+    swiper.setAttribute('slides-per-view', '1');
+  } else {
+    swiper.setAttribute('slides-per-view', '2.5');
+  }
+});
