189e06252bd49b94263a55184485d31ceda9826c
Natália Santos <tanatelha.dados@gmail.com>
2023-10-25 23:25:18 -0300

    Responsividade usando JS

--- script-experimentos.js
+++ script-experimentos.js
@@ -18,3 +18,16 @@ const swiper = new Swiper('.swiper', {
       el: '.swiper-scrollbar',
     },
   });
+
+/*Responsividade usando JS*/
+function adjustSlidesPerView() {
+        if (window.innerWidth <= 400) {
+            document.querySelector('.swiper-container').setAttribute('slides-per-view', '1.5');
+        } else {
+            document.querySelector('.swiper-container').setAttribute('slides-per-view', '2.5');
+        }
+    }
+
+    // Chame a função quando a página carregar e quando a janela for redimensionada
+    window.addEventListener('load', adjustSlidesPerView);
+    window.addEventListener('resize', adjustSlidesPerView); 
